dd8287b8120f151245bf4c5503ab06b6fe99670c
John Kester <nnamanikester@gmail.com>
2020-04-26 19:40:18 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useKeepAwake } from 'expo-keep-awake';
+import HomeScreen from "./src/screens/Home";
+import SecondScreen from "./src/screens/Second";
+
+const Stack = createStackNavigator();
+
+function App() {
+  useKeepAwake();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: "#6c63ff"
+        },
+        headerTitleStyle: {
+          fontSize: 20
+        },
+        headerTintColor: "#ffffff"
+      }
+      }>
+        <Stack.Screen name="EarnBigTime" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer >
+  );
+}
+
+export default App;
---
+++ src/screens/Second.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { View, Button } from "react-native";
+import { AdMobBanner, AdMobInterstitial } from "expo-ads-admob"
+
+AdMobInterstitial.setAdUnitID('ca-app-pub-9699079901863001/9547446409');
+// AdMobInterstitial.setTestDeviceID('EMULATOR')
+
+export default class SecondScreen extends Component {
+  state = {
+    isInterstitial: false
+  }
+
+  loadInterstitial = async () => {
+    try {
+      this.setState({ isInterstitial: true })
+      await AdMobInterstitial.requestAdAsync();
+      await AdMobInterstitial.showAdAsync();
+      setInterval(() => {
+        this.props.navigation.navigate("EarnBigTime");
+      }, 10000);
+    }
+    catch (e) {
+      console.log(e);
+    } finally {
+      this.setState({ isInterstitial: false });
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.loadInterstitial();
+    }, 15000)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ marginBottom: 20 }}>
+          <AdMobBanner
+            bannerSize="banner"
+            adUnitID="ca-app-pub-9699079901863001/9046605722"
+          />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <AdMobBanner
+            bannerSize="fullBanner"
+            adUnitID="ca-app-pub-9699079901863001/7584462197"
+          />
+        </View>
+        <Button title="Load" disabled={this.isInterstitial} style={styles.button} onPress={() => this.loadInterstitial()} />
+        <View style={{ marginTop: 20 }}>
+          <AdMobBanner
+            bannerSize="leaderboard"
+            adUnitID="ca-app-pub-9699079901863001/1720927270"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+// ca-app-pub-9699079901863001/1720927270 -> my-banner
+// ca-app-pub-9699079901863001/9046605722 -> my-banner-1
+// ca-app-pub-9699079901863001/7584462197 -> my-banner-2
+// ca-app-pub-9699079901863001/9547446409 -> my-interstitial
+// ca-app-pub-9699079901863001/4503003288 -> my-interstitial-1
+// ca-app-pub-9699079901863001/3189921612 -> my-interstitial-2
+
+const styles = {
+  button: {
+    width: 30,
+    height: 50,
+    marginTop: 20
+  },
+  container: {
+    justifyContent: "center",
+    padding: 20
+  }
+}
