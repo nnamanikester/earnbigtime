78052352f8213e7a410bce7408350235d66a98b9
kescript <nnamanikester@gmail.com>
2020-05-04 11:07:38 +0100

    Built for kester

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { View, Button } from "react-native";
-import { AdMobBanner, AdMobInterstitial } from "expo-ads-admob"
+import { AdMobBanner, AdMobInterstitial } from "expo-ads-admob";
 
-AdMobInterstitial.setAdUnitID('ca-app-pub-1565969270023875/8925603640');
+AdMobInterstitial.setAdUnitID("ca-app-pub-2903800103454363/4475362362");
 
 export default class HomeScreen extends Component {
   state = {
-    isInterstitial: false
-  }
+    isInterstitial: false,
+  };
 
   loadInterstitial = async () => {
     try {
-      this.setState({ isInterstitial: true })
+      this.setState({ isInterstitial: true });
       await AdMobInterstitial.requestAdAsync();
       await AdMobInterstitial.showAdAsync();
       setInterval(() => {
         this.props.navigation.navigate("Second");
       }, 10000);
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
     } finally {
       this.setState({ isInterstitial: false });
     }
-  }
+  };
 
   componentDidMount() {
     setInterval(() => {
       this.loadInterstitial();
-    }, 15000)
+    }, 15000);
   }
 
   render() {
@@ -37,20 +36,25 @@ export default class HomeScreen extends Component {
         <View style={{ marginBottom: 20 }}>
           <AdMobBanner
             bannerSize="banner"
-            adUnitID="ca-app-pub-1565969270023875/5341689658"
+            adUnitID="ca-app-pub-2903800103454363/7484669083"
           />
         </View>
         <View style={{ marginBottom: 20 }}>
           <AdMobBanner
             bannerSize="fullBanner"
-            adUnitID="ca-app-pub-5344537221584334/5673239449"
+            adUnitID="ca-app-pub-2903800103454363/7484669083"
           />
         </View>
-        <Button title="Load" disabled={this.isInterstitial} style={styles.button} onPress={() => this.loadInterstitial()} />
+        <Button
+          title="Load"
+          disabled={this.isInterstitial}
+          style={styles.button}
+          onPress={() => this.loadInterstitial()}
+        />
         <View style={{ marginTop: 20 }}>
           <AdMobBanner
             bannerSize="leaderboard"
-            adUnitID="ca-app-pub-1565969270023875/5341689658"
+            adUnitID="ca-app-pub-2903800103454363/7484669083"
           />
         </View>
       </View>
@@ -58,19 +62,14 @@ export default class HomeScreen extends Component {
   }
 }
 
-// ca-app-pub-1317983810707336/3087416623 -> interstitial-1
-// ca-app-pub-1317983810707336/8526611417 -> interstitial-2
-// ca-app-pub-1317983810707336/3734090363 -> banner-1
-// ca-app-pub-1317983810707336/3900491762 -> banner-2
-
 const styles = {
   button: {
     width: 30,
     height: 50,
-    marginTop: 20
+    marginTop: 20,
   },
   container: {
     justifyContent: "center",
-    padding: 20
-  }
-}
+    padding: 20,
+  },
+};
