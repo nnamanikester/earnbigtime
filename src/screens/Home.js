2d63586913e9dae50b28466097e84b647bf5306f
kescript <nnamanikester@gmail.com>
2020-05-04 19:28:18 +0100

    Built for Nobbs

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View, Button } from "react-native";
 import { AdMobBanner, AdMobInterstitial } from "expo-ads-admob";
 
-AdMobInterstitial.setAdUnitID("ca-app-pub-4128130523178083/1271448675");
+AdMobInterstitial.setAdUnitID("ca-app-pub-6026195991662344/8904711901");
 
 export default class HomeScreen extends Component {
   state = {
@@ -36,13 +36,13 @@ export default class HomeScreen extends Component {
         <View style={{ marginBottom: 20 }}>
           <AdMobBanner
             bannerSize="banner"
-            adUnitID="ca-app-pub-4128130523178083/3187165574"
+            adUnitID="ca-app-pub-6026195991662344/6737358349"
           />
         </View>
         <View style={{ marginBottom: 20 }}>
           <AdMobBanner
             bannerSize="fullBanner"
-            adUnitID="ca-app-pub-4128130523178083/3187165574"
+            adUnitID="ca-app-pub-6026195991662344/6737358349"
           />
         </View>
         <Button
@@ -54,7 +54,7 @@ export default class HomeScreen extends Component {
         <View style={{ marginTop: 20 }}>
           <AdMobBanner
             bannerSize="leaderboard"
-            adUnitID="ca-app-pub-4128130523178083/3187165574"
+            adUnitID="ca-app-pub-6026195991662344/6737358349"
           />
         </View>
       </View>
